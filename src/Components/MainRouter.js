cdbd36bd298e1bac548a3bd79936c0735d747603
Brandon SORET <brandonsoret@hotmail.fr>
2020-01-13 10:59:20 +0100

    Initialize Main Router

---
+++ src/Components/MainRouter.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function MainRouter() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Utilisateurs</Link>
+          </li>
+          <li>
+            <Link to="/logout">Déconnexion</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/">
+          </Route>
+          <Route path="/users">
+          </Route>
+          <Route path="/logout">
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
