742b41f0f49d706a5dec4a85f084a6dd9acddbe1
Brandon SORET <brandonsoret@hotmail.fr>
2020-01-15 19:17:13 +0100

    remove TOGGLE_CONNECTION from reducers and actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,3 @@
-export const toggleConnection = () => ({
-        type: "TOGGLE_CONNECTION"
-});
-
 export const connectUser = (data) => ({
         type: "CONNECT_USER",
         userData: data
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,9 +2,6 @@ function reducer(state, action) {
     const newState = {...state};
 
     switch (action.type) {
-        case "TOGGLE_CONNECTION":
-            newState.isConnected = !newState.isConnected;
-            return newState;
         case "CONNECT_USER":
             newState.id = action.userData.id;
             newState.token = action.userData.token;
