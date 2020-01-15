246961a9edb10c9a80fdb63b8ec53e5640f2bc17
Brandon SORET <brandonsoret@hotmail.fr>
2020-01-15 15:53:06 +0100

    add tittle for listUser

--- src/Components/ListUsers.js
+++ src/Components/ListUsers.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import '../ComponentsCSS/listUsers.css';
+
+function Users(props){
+    return(
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
 class ListUsers extends React.Component{
     render(){
         return(
             <>
-
+                <div id="wrapper-tittle">
+                    <span>Utilisateurs connectés :</span>
+                </div>
+                <Users/>
             </>
         )
     }
