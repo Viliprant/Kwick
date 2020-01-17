5556ad12c9d835269f02b70c37d4ba7417312ffa
Brandon SORET <brandonsoret@hotmail.fr>
2020-01-17 14:17:14 +0100

    export function message from chat

---
+++ src/Components/Message.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+
+function Message(props){
+
+    const isToday = (date) => {
+        const today = new Date();
+        return date.getDate() === today.getDate() &&
+            date.getMonth() === today.getMonth() &&
+            date.getFullYear() === today.getFullYear()
+      }
+
+    const convertTimestampToDate = (timestamp) => {
+        const date = new Date(timestamp * 1000); // en milisecondes
+        let dateToReturn = date;
+        if(isToday(date))
+        {
+            const options = { hour: 'numeric', minute:'numeric' };
+            dateToReturn = date.toLocaleDateString('fr-FR', options);
+            dateToReturn = dateToReturn.slice(dateToReturn.length - 5);
+        }
+        else{
+            const options = { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute:'numeric' };
+            dateToReturn = date.toLocaleDateString('fr-FR', options);
+        }
+        return dateToReturn;
+    }
+
+    return(
+        <div className={"wrapper-messages " + (props.isOwnMessage ? 'own-messages' : 'other-messages')}>
+            <div className="author-message">
+                {props.isOwnMessage ? '' : props.infosMessages.user_name}
+            </div>
+            <div style={{position:"relative"}}>
+                <p className="content-message">
+                    {props.infosMessages.content}
+                </p>
+                <div className="triangle"></div>
+            </div>
+            
+            <div className="date-message">
+                {convertTimestampToDate(props.infosMessages.timestamp)}
+            </div>
+        </div>
+    )
+}
+
+export default Message;
